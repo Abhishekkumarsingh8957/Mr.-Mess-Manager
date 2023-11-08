fc3f35243b440ec73f168446f3ac2ad62ca7144e
Vinay <97828541+Kumarvinay710@users.noreply.github.com>
2023-11-08 23:18:36 +0530

    Add files via upload
    login And Register Page Added

---
+++ Auth/Register.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+import "../../styles/AuthStyles.css";
+
+
+const Register = () => {
+  return (
+
+
+<Layout title="Register - Ecommer App">
+<div className="form-container ">
+  <form >
+    <h4 className="title">STUDENT</h4>
+    
+    <div className="mb-3">
+      <input
+        type="text"
+        
+        className="form-control"
+        id="exampleInputEmail1"
+        placeholder="Enter Your Name"
+        required
+        autoFocus
+      />
+    </div>
+     
+    <div className="mb-3">
+      <input
+        type="text"
+        
+        className="form-control"
+        id="exampleInputEmail1"
+        placeholder="Enter Your Reg.No."
+        required
+        autoFocus
+      />
+    </div>
+
+    <div className="mb-3">
+      <input
+        type="email"
+       
+        className="form-control"
+        id="exampleInputEmail1"
+        placeholder="Enter Your Email "
+        required
+      />
+    </div>
+
+    <div className="mb-3">
+      <input
+        type="text"
+        
+        className="form-control"
+        id="exampleInputEmail1"
+        placeholder="Enter Your Hostel"
+        required
+        autoFocus
+      />
+    </div>
+    <div className="mb-3">
+      <input
+        type="text"
+        
+        className="form-control"
+        id="exampleInputEmail1"
+        placeholder="Enter Your Mob.No."
+        required
+        autoFocus
+      />
+    </div>
+    <div className="mb-3">
+      <input
+        type="password"
+      
+        className="form-control"
+        id="exampleInputPassword1"
+        placeholder="Enter Your Password"
+        required
+      />
+    </div>
+   
+    
+    
+    <button type="submit" className="btn btn-primary">
+      REGISTER
+    </button>
+  </form>
+</div>
+</Layout> 
+
+
+
+
+
+
+
+
+  )
+}
+
+export default Register
---
+++ Auth/RegisterA.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+import "../../styles/AuthStyles.css";
+
+
+const RegisterA = () => {
+  return (
+    <Layout title="Register - Ecommer App">
+    <div className="form-container ">
+      <form >
+        <h4 className="title">ACCOUNTANT </h4>
+        
+        <div className="mb-3">
+          <input
+            type="text"
+            
+            className="form-control"
+            id="exampleInputEmail1"
+            placeholder="Enter Your Name"
+            required
+            autoFocus
+          />
+        </div>
+         
+       
+    
+        <div className="mb-3">
+          <input
+            type="text"
+            
+            className="form-control"
+            id="exampleInputEmail1"
+            placeholder="Enter Your Hostel"
+            required
+            autoFocus
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            
+            className="form-control"
+            id="exampleInputEmail1"
+            placeholder="Enter Your Mob.No."
+            required
+            autoFocus
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+          
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Enter Your Password"
+            required
+          />
+        </div>
+       
+        
+        
+        <button type="submit" className="btn btn-primary">
+          REGISTER
+        </button>
+      </form>
+    </div>
+    </Layout> 
+    
+    
+  )
+}
+
+export default RegisterA
