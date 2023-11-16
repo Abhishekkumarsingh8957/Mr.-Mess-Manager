451a9ff85361435f75dcc2d15785b5a97bb57236
Vinay <97828541+Kumarvinay710@users.noreply.github.com>
2023-11-16 22:36:37 +0530

    Add files via upload
    Main Server File

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
-const http=require('http');
-const app=require('./app');
-const server=http.createServer(app);
-server.listen(8080,console.log('app is working fine'));
+
+import express from 'express';
+import colors from 'colors';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js'
+import cors from 'cors';
+
+connectDB();
+
+const app=express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:3000",
+    method:["GET","POST","DELETE"]
+}));
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Project");
+})
+
+const PORT=8080;
+
+app.use("/api/v1/auth",authRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Server is Running On port ${PORT}`.bgCyan.white);
+})
